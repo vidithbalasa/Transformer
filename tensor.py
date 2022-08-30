7d37ffe53e4a61812d0ac581e6789c37d5ffc644
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-08-29 20:59:15 -0700

    Fixed transpose and reshape functions

--- tensor.py
+++ tensor.py
@@ -1,20 +1,6 @@
 '''
-Apparently, all you need for a tensor is a list with a couple helper functions.
+Apparently all you need for a tensor is a list with a couple helper functions.
 '''
-
-# def is_valid_tensor(l: list) -> bool:
-#     '''
-#     Given an n-dimensional list, return True if it is a valid tensor.
-
-#     A valid tensor is a n-dimentional list where the last dimention (n[-1]) is
-#     the same for all elements.
-
-#     This function recursively checks the dimensions of the list
-#     '''
-#     if not isinstance(l[0], list):
-#         return True
-#     dim = 0
-#     return    
 
 def shape(l: list) -> tuple:
     '''
@@ -24,11 +10,24 @@ def shape(l: list) -> tuple:
         return (len(l),)
     return (len(l),) + shape(l[0])
 
-def flatten_2d(l: list) -> list:
+def flatten_nd(l: list) -> list:
     '''
-    Flatten a list of lists into a single list.
+    Flatten an n-dimentional list.
     '''
-    return [item for sublist in l for item in sublist]
+    x = l.copy()
+    while isinstance(x[0], list):
+        x = [item for sublist in x for item in sublist]
+    return x
+
+def reshape_flattened_nd(l: list, og_shape: tuple) -> list:
+    '''
+    Reshape a flattened list into an n-dimensional list.
+    '''
+    matrix = l.copy()
+    if len(og_shape) == 1:
+        return matrix
+    splits = len(matrix) // og_shape[0]
+    return [reshape_flattened_nd(matrix[x:x+splits], og_shape[1:]) for x in range(0, len(matrix), splits)]
 
 def view_by_head(l: list, d_k: int) -> list:
     '''
@@ -45,11 +44,5 @@ def view_by_head(l: list, d_k: int) -> list:
             x = [token[x:x+d_k] for x in range(0, len(token), d_k)]
             new_seq.append(x)
         matrix.append(new_seq)
-        
-    return [list(token) for seq in matrix for token in zip(*seq)]
-
-def reshape_flattened_2d(l: list, shape: tuple) -> list:
-    '''
-    Reshape a flattened list into a 2-dimensional list.
-    '''
-    return [l[i:i+shape[1]] for i in range(0, len(l), shape[1])]
+    
+    return [list(map(list, zip(*seq))) for seq in matrix]
