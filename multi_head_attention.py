6594df8eb67f27ca5d9477809b905c7ae7d0ec54
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-09-01 20:21:17 -0700

    finished multihead attention

--- multi_head_attention.py
+++ multi_head_attention.py
@@ -1,15 +1,15 @@
 import math
 from linear import Linear
-from tensor import matmul_4d
+from tensor import add_tensors, apply, flatten_nd, matmul_4d, shape, softmax, transpose, zeros_from
 
 class MultiHeadAttention:
-    def __init__(self, d_model = 512, n_head = 8):
+    def __init__(self, d_model = 512, n_head = 8, max_seq_len = 512):
         '''
         Params
         ------
         @d_model: embedding size
         @n_head: number of heads        
-        d_k: embedding size after paritioning the output by n_head
+        d_k: embedding size after paritioning the embedding vector by n_head
 
         Layers
         ------
@@ -20,7 +20,7 @@ class MultiHeadAttention:
         self.n_head = n_head
         self.d_k = d_model // n_head
 
-        in_out = (self.d_model, self.d_model) 
+        in_out = (self.d_model * max_seq_len,) * 2 
         self.W_Q = Linear(*in_out)
         self.W_K = Linear(*in_out)
         self.W_V = Linear(*in_out)
@@ -33,13 +33,20 @@ class MultiHeadAttention:
         Forward pass
         Assume Q, K, and V are all word embeddings of shape (batch_size, sequence_length, embedding_size)
 
+        Steps
+        -----
         1. Run each sequence (Q, K, V) through the corresponding linear layers
            - Spits out 3 matrices of shape (batch_size, sequence_length, embedding_size) 
         2. Transpose the matrix from 
             (batch_size, sequence_length, embedding_size) -> (batch_size, n_heads, sequence_length, d_k)
         3. Get attention score per head
-        4. Run the output of all the heads through output layers
+        4. Concatenate attention scores
+            - (b, h, l, d) -> (b, l, h, d) -> (b, l, h*d)
+        5. Run attention scores through the output layers
+        6. Add the residual connection
         '''
+        input = Q
+
         Q = [self.W_Q(q) for q in Q]
         K = [self.W_K(k) for k in K]
         V = [self.W_V(v) for v in V]
@@ -50,33 +57,40 @@ class MultiHeadAttention:
 
         x = self.ScaledDotProductAttention(Q, K, V)
 
+        x = [list(map(list, zip(*seq))) for seq in x] # (b,h,l,d) -> (b,l,h,d)
+        # (b,l,h,d) -> (b,l,h*d)
+        for idx in range(len(x)):
+            x[idx] = [flatten_nd(seq) for seq in x[idx]]
+
         x = [self.W_O1(i) for i in x]
         x = [self.W_O2(i) for i in x]
 
-        return x
+        return add_tensors(input, x)       
 
     def ScaledDotProductAttention(self, Q, K, V, mask=None) -> list:
         '''
-        Assume Q,K,V are all matrices of shape (batch_size, n_heads, sequence_length, d_k)
+        Assume Q,K,V are all matrices of shape (batch_size, n_heads, sequence_length, d_k) == (b,h,l,d)
 
+        Equation
+        --------
         Attention(Q,K,V) = softmax( (Q * K^T) / sqrt(d_k) ) * V
         '''
-        K_T = K # (b,h,l,d) -> (b,h,d,l)
+        K_T = zeros_from(shape(K)) # K -> K^T || (b,h,l,d) -> (b,h,d,l)
         for i in range(len(K)):
             for j in range(len(K[0])):
-                K_T[i][j] = [list(s) for s in zip(*K[i][j])]
+                K_T[i][j] = transpose(K[i][j])
 
         x = matmul_4d(Q, K_T) # Q * K^T = x || shape(x) = (b,h,l,l)
-        x = [i/math.sqrt(len(K)) for i in x] # x / sqrt(d_k)
 
-        scale = sum([math.exp(i.data) for i in x])
-        if scale:
-            x = [Value(math.exp(i.data)) / scale for i in x] # softmax(x)
+        scale = math.sqrt(len(K))
+        x = apply(x, lambda i: i / scale) # x / sqrt(d_k)
+
+        x = softmax(x)
 
         if mask is not None:
-            x = x * mask
+            x += mask
 
-        x = [xi * vi for xi, vi in zip(x, V)] # x • V
+        x = matmul_4d(x, V) # x • V
         
         return x
     
--- tensor.py
+++ tensor.py
@@ -3,6 +3,7 @@ Apparently all you need for a tensor is a list with a couple helper functions.
 '''
 
 import math
+from micrograd.engine import Value
 
 
 def shape(l: list) -> tuple:
@@ -27,14 +28,16 @@ def reshape_flattened_nd(l: list, og_shape: tuple) -> list:
     Reshape a flattened list into an n-dimensional list.
     '''
     matrix = l.copy()
+    if shape(matrix) == og_shape:
+        return matrix
     if len(og_shape) == 1:
         return matrix
-    splits = len(matrix) // og_shape[0]
+    splits = math.ceil(len(matrix) / og_shape[0])
     return [reshape_flattened_nd(matrix[x:x+splits], og_shape[1:]) for x in range(0, len(matrix), splits)]
 
 def matmul_4d(a: list, b: list) -> list:
     '''
-    Matrix multiplication of two 4-dimensional lists.
+    Matrix multiplication of two 4-dimensional tensors.
 
     Matrix math from "Multidimensional Matrix Math" by Ashu M. G. Solo:
     http://www.iaeng.org/publication/WCE2010/WCE2010_pp1829-1833.pdf
@@ -53,8 +56,7 @@ def matmul_4d(a: list, b: list) -> list:
     assert a_shape[:-2] == b_shape[:-2], "3rd and 4th dimensions of matrix A must match 3rd and 4th dimensions of matrix B"
 
     new_shape = (a_shape[0], a_shape[1], a_shape[2], b_shape[-1])
-    zeros = [0] * math.prod(new_shape)
-    zeros = reshape_flattened_nd(zeros, new_shape)
+    zeros = zeros_from(new_shape)
 
     for l in range(new_shape[0]):
         for k in range(new_shape[1]):
@@ -63,3 +65,48 @@ def matmul_4d(a: list, b: list) -> list:
                     zeros[l][k][i][j] = sum([a[l][k][i][x] * b[l][k][x][j] for x in range(len(a[l][k][i]))])
     
     return zeros
+
+def zeros_from(shape: tuple) -> list:
+    '''
+    Returns a list of zeros with the given shape.
+    '''
+    zeros = [0] * math.prod(shape)
+    return reshape_flattened_nd(zeros, shape)
+
+def transpose(l: list) -> list:
+    '''
+    Transpose a 2d list.
+    '''
+    return [list(x) for x in zip(*l)]
+
+def apply(l: list, f: callable) -> list:
+    '''
+    Apply a function to every element of an n-dimensional list.
+    '''
+    stored_shape = shape(l)
+    applied = [f(x.data) if isinstance(x, Value) else Value(f(x)) for x in flatten_nd(l)]
+    return reshape_flattened_nd(applied, stored_shape)
+
+def tensor_sum(l: list) -> Value:
+    '''
+    Sum all elements of an n-dimensional list.
+    '''
+    return sum(flatten_nd(l))
+
+def softmax(l: list) -> list:
+    '''
+    Get the softmax of an n-dimensional list.
+    '''
+    stored_shape = shape(l)
+    exp = apply(l, math.exp)
+    exp_sum = tensor_sum(exp)
+    softmax = apply(exp, lambda x: x / exp_sum)
+    return reshape_flattened_nd(softmax, stored_shape)
+
+def add_tensors(l1: list, l2: list) -> list:
+    '''
+    Add two n-dimensional lists.
+    '''
+    stored_shape = shape(l1)
+    added = [x + y for x, y in zip(flatten_nd(l1), flatten_nd(l2))]
+    return reshape_flattened_nd(added, stored_shape)
